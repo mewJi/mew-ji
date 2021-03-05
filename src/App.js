9b11890d9ef87109db02dd48e8e885a56cd75e1d
NICHAPHAT <meme.doubleu@gmail.com>
2021-03-05 23:17:15 +0700

    test edit text in app page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. mewji box
+          Edit <code>src/App.js</code> and save to reload. mewji box ---
         </p>
         <a
           className="App-link"
